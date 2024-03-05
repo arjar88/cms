8eefc7ab1fd7a78983226f396a4cd4c6cef00f53
arjar88 <superaryeh98@gmail.com>
2024-03-05 13:50:02 +0200

    Update genericControllers.js

--- src/controllers/genericControllers.js
+++ src/controllers/genericControllers.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
 const Client = require("../models/Client");
-const path = require("path");
-const Client = require(path.resolve(__dirname, "../models/Client"));
 const Object = require("../models/Object");
 const Properties = require("../models/Property");
 const Data = require("../models/Data");
